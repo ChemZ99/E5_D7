ff49ed6847c46c4445fb571fbefc1fcb2bce3e9b
Andrea Cavallo <astarore@gmail.com>
2023-11-07 15:10:05 +0100

    Initial commit

---
+++ src/main/java/Exercises/E5_D7/E5D7Application.java
@@ -0,0 +1,13 @@
+package Exercises.E5_D7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E5D7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E5D7Application.class, args);
+	}
+
+}
